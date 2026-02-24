158480fc4a1b5b58f01ddcf3183e2c468ec3ee68
yumyeongju <yumyeongjoo@naver.com>
2026-02-24 13:06:20 +0900

    Refactor main.py to be implemented in ui.py and ctrl.py

--- main.py
+++ main.py
@@ -1,51 +1,15 @@
-# ch 4.4.1 main.py
+#ch 5.2.1 main.py
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout,
-                             QMessageBox, QPlainTextEdit, QHBoxLayout)   
-from PyQt5.QtGui import QIcon   
+from ui import View #ui.py의 View 클래스 추가
+from ctrl import Control    #ctrl.py의 Control 클래스 추가
+from PyQt5.QtWidgets import QApplication
 
-class Calculator(QWidget):
-    
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.te1 = QPlainTextEdit()  #텍스트 에디트 위젯 생성
-        self.te1.setReadOnly(True)  #텍스트 에디트 위젯을 읽기만 가능하도록 수정
-
-        self.btn1=QPushButton('Message', self)  
-        self.btn1.clicked.connect(self.activateMessage) 
-
-        self.btn2=QPushButton('Clear', self)  
-        self.btn2.clicked.connect(self.clearMessage)
-
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(self.btn1)
-        hbox.addWidget(self.btn2) 
-
-        vbox=QVBoxLayout()  
-        vbox.addWidget(self.te1)   #수직 레이아웃에 텍스트 에디트 위젯 추가
-        #vbox.addWidget(self.btn1)
-        vbox.addLayout(hbox)
-        vbox.addStretch(1)  
-
-        self.setLayout(vbox)    #빈 공간 - 버튼 - 빈 공간 순으로 수직 배치된 레이아웃 설정
-
-        self.setWindowTitle('Calculator')
-        self.setWindowIcon(QIcon('icon.png'))   #윈도 아이콘 추가
-        self.resize(256, 256)
-        self.show()
-
-    def activateMessage(self):  #핸들러 함수 수정 : 메세지가 텍스트 에디트에 출력되도록
-        # QMessageBox.information(self, "information", "Button clicked!")
-        self.te1.appendPlainText("Button clicked!")
-
-    def clearMessage(self):
-        self.te1.clear()
-        
-if __name__ == '__main__':
+def main(): #프로그램 실행(Application) 관련 내용 함수화
+    calc = QApplication(sys.argv)
     app=QApplication(sys.argv)
-    view=Calculator()
+    view=View() #View 인스턴스 선언
+    Control(view=view)  #Control 인스턴스 선언
     sys.exit(app.exec_())
+
+if __name__=='__main__':
+    main()
