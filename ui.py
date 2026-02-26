ba8269eaf4a0470f5a6b55dcf2d5e414628df6dd
developer2 <dev2@email.com>
2026-02-26 17:19:16 +0900

    Modify btn1, le1 and method name to setDisplay in ui.py

--- ui.py
+++ ui.py
@@ -1,4 +1,4 @@
-#ch 6.2.1 ui.py
+#ch 6.3.1 ui.py
 from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout,
                              QMessageBox, QPlainTextEdit, QHBoxLayout,
                              QLineEdit, QComboBox)
@@ -15,11 +15,13 @@ class View(QWidget):
         self.te1 = QPlainTextEdit()
         self.te1.setReadOnly(True)
 
-        self.btn1=QPushButton('Message', self)
+        self.btn1=QPushButton('Calc', self)
         self.btn2=QPushButton('Clear', self)
 
         self.le1=QLineEdit('0', self)
         self.le1.setAlignment(QtCore.Qt.AlignRight)
+        self.le1.setFocus(True)
+        self.le1.selectAll()
 
         self.le2=QLineEdit('0', self)
         self.le2.setAlignment(QtCore.Qt.AlignRight)
@@ -50,7 +52,7 @@ class View(QWidget):
         self.resize(256, 256)
         self.show()
 
-    def activateMessage(self):
+    def setDisplay(self):
         self.te1.appendPlainText("Button clicked!")
 
     def clearMessage(self):
