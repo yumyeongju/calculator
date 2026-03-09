b66d3d0e514bf8c4b852f4b297caa76c9570cdbc
yumyeongju <yumyeongjoo@naver.com>
2026-03-09 22:59:17 +0900

    Modify calculate function in ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -1,4 +1,4 @@
-#ch 7.1.3 ctrl.py
+#ch 7.6.2 ctrl.py
 class Control:
 
     def __init__(self, view):
@@ -6,14 +6,24 @@ class Control:
         self.connectSignals()
 
     def calculate(self):
-        num1 = float(self.view.le1.text())
-        num2 = float(self.view.le2.text())
-        operator = self.view.cb.currentText()
+        try:
+            num1 = float(self.view.le1.text())
+            num2 = float(self.view.le2.text())
+            operator = self.view.cb.currentText()
 
-        if operator =='+':
-            return f'{num1} + {num2} = {self.sum(num1, num2)}'
-        
-        else:
+            if operator == '+':
+                return f'{num1} + {num2} = {self.sum(num1, num2)}'
+            elif operator == '-':
+                return f'{num1} - {num2} = {self.sub(num1, num2)}'
+            elif operator == '*':
+                return f'{num1} * {num2} = {self.mul(num1, num2)}'
+            elif operator == '/':
+                return f'{num1} / {num2} = {self.div(num1, num2)}'
+            elif operator == '^':
+                return f'{num1} ^ {num2} = {self.pow(num1, num2)}'
+            else :
+                return "Calculation Error"
+        except : 
             return "Calculation Error"
 
     def connectSignals(self):
