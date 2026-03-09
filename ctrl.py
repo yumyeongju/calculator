d959bebaaf8a5b6efa5a27b857d1922fc989e59d
yumyeongju <yumyeongjoo@naver.com>
2026-03-09 23:02:21 +0900

    Add mod function and modify calculate function in ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -21,6 +21,8 @@ class Control:
                 return f'{num1} / {num2} = {self.div(num1, num2)}'
             elif operator == '^':
                 return f'{num1} ^ {num2} = {self.pow(num1, num2)}'
+            elif operator == '%':
+                return f'{num1} % {num2} = {self.mod(num1, num2)}'
             else :
                 return "Calculation Error"
         except : 
@@ -45,3 +47,13 @@ class Control:
     
     def pow(self, a, b):
         return pow(a,b)
+    
+    def mod(self, a, b):
+        try:
+            if(b==0):
+                raise Exception("Divisor Error")
+        
+        except Exception as e:
+            return e
+        
+        return a%b 
